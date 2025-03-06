d6978668fcacf02dd9c1dc74f151c01f35552b1b
Shalombonet <shalom.bonet@binghamuni.edu.ng>
2025-03-06 15:44:09 -0800

    This is gthe first commit.

---
+++ src/main/java/edu/tcu/cs/deliveryappassignment/DeliveryAppAssignmentApplication.java
@@ -0,0 +1,13 @@
+package edu.tcu.cs.deliveryappassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryAppAssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeliveryAppAssignmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/tcu/cs/deliveryappassignment/DeliveryAppAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.tcu.cs.deliveryappassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryAppAssignmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
